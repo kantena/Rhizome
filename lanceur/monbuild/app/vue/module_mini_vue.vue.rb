e0d9a1063a9e69a67c0c85dfadb7fa466f076352
Jean-Luc Andrei <jean-luc@kantena.com>
2010-08-23 17:25:20 +0200

    suite deploiement pas fini

--- lanceur/monbuild/app/vue/module_mini_vue.vue.rb
+++
@@ -1,8 +0,0 @@
-class Module_mini_vue < Module_launcher
-  def initialize arg
-    super()
-    jrhizette.set_text(arg.titre)
-    jblabla.set_text(arg.description)
-    btn_lance.add_action_listener {|evt| NavigationController.navigation.launch_module(arg.action)}
-  end
-end
